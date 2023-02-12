f47c3344a979be64f7941716a8d5eae58e2a3b77
Paolo Oranges <paolo.oranges@gmail.com>
2023-02-12 19:18:06 +0100

    first commit on mac

---
+++ UtilitiesForDockerImage/DownloadDataUntilNow.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+LAST_DOWNLOAD_SUCCESS_TIME_FILE=last_success_time
+TICKERS_FILE=tickers
+
+START_TIME=
+END_TIME=$(date +%Y%m%d-%H:%M:%S) # now
+
+TICKERS=
+
+DOWLOADER_PATH=.
+while getopts p:t: flag
+do
+    case "${flag}" in
+        p) DOWLOADER_PATH=${OPTARG};;
+        t) TICKERS_FILE=${OPTARG};;
+    esac
+done
+
+if [ ! -f $TICKERS_FILE ]
+then
+    echo "No Tickers File! ERROR!"
+    exit -1
+fi
+
+TICKERS=$(<$TICKERS_FILE)
+# echo $TICKERS
+
+if [ ! -f $LAST_DOWNLOAD_SUCCESS_TIME_FILE ]
+then
+    START_TIME=20210101-00:00:00
+    echo "No previous success file found, start from: " $START_TIME
+else
+    START_TIME=$(<$LAST_DOWNLOAD_SUCCESS_TIME_FILE)    
+fi
+
+# echo "Start: " $START_TIME
+# echo "End: " $END_TIME
+echo $DOWLOADER_PATH
+
+RESOLUTIONS=("Minute" "Hour" "Daily")
+
+for res in ${RESOLUTIONS[@]}; do
+  echo "Start Downloading for " $res
+  dotnet $DOWLOADER_PATH/QuantConnect.GDAXBrokerage.ToolBox.dll --app=download --tickers=$TICKERS --resolution=$res --from-date=$START_TIME --to-date=$END_TIME
+done
+
+echo $END_TIME > $LAST_DOWNLOAD_SUCCESS_TIME_FILE
---
+++ UtilitiesForDockerImage/EntryPoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CRON_FILE="downloader-crontab"
+
+echo "Loading crontab file: $CRON_FILE"
+
+# Load the crontab file
+crontab $CRON_FILE
+
+# Start cron
+echo "Starting cron..."
+crond -f
+
